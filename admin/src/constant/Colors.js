33a7ca174facee043e56b19eafe858e7085ff95c
Starbox <39715563+Starbox7@users.noreply.github.com>
2023-03-25 13:56:31 +0900

    Mar. 25. 2023 - 컬러 팔레트 코드화

---
+++ admin/src/constant/Colors.js
@@ -0,0 +1,37 @@
+export const BackgroundColor = {
+    bcGreen1 : '#E0F0E1',
+    bcGreen2 : '#C4E1C5',
+    bcYellow : '#FFF1D7'
+}
+
+export const DetailBackgroundColor = {
+    dbcGreen1 : '#E0F0E1',
+    dbcGreen2 : '#AED0AF',
+    dbcGreen3 : '#627B63',
+    dbcYellow1 : '#FFF1D7',
+    dbcYellow2 : '#FFDE9D'
+}
+
+export const BorderColor = {
+    bdcGreen1 : '#7F9980',
+    bdcGreen2 : '#1F3320'
+}
+
+export const IconColor = {
+    icGreen : '#1F3320',
+    icYellow : '#FAB329'
+}
+
+export const TextColor = {
+    tcGreen1 : '#627B63',
+    tcGreen2 : '#1F3320',
+    tcBrown1 : '#8C8175',
+    tcBrown2 : '#2D1A04',
+    tcGray : '#D9D9D9',
+}
+
+export const ShadowColor = {
+    scGreen : '#97A997',
+    scYellow : '#DEBA73',
+    scGray : '#BBBBBB'
+}
