5c49ec86f426b29ef42bcfbe981aca2afae2acd2
Starbox <39715563+Starbox7@users.noreply.github.com>
2023-04-05 01:02:36 +0900

    Apr. 05. 2023 - Sign In

--- src/data/repository/auth.js
+++ src/data/repository/auth.js
@@ -1,15 +1,13 @@
-import axios from "axios";
+import axios from 'axios';
 
 const auth = {
-  signUp: async (signUpData) => {
+  signUp: async (signData) => {
     try {
-      return await axios.post('http://127.0.0.1:5001/auth/signup', signUpData);
+      return await axios.post('http://127.0.0.1:5001/auth/signup', signData);
     } catch (err) {
       /** Test!!! */ console.log(`signUp : ${err}`);
+      throw new Error('axios Error : signUp');
     }
-
-
-    /** Test!!! */ return console.log(`here is auth Repository`);
   },
 };
 
--- src/store/authStore.js
+++ src/store/authStore.js
@@ -2,7 +2,7 @@ import { create } from 'zustand';
 import auth from '../data/repository/auth';
 
 const authStore = create((set, get) => ({
-  signUpData: {
+  signData: {
     id: '',
     password: '',
     confirm: '',
@@ -13,87 +13,120 @@ const authStore = create((set, get) => ({
   isLoading: false,
 
   setSignData: (targetId, targetValue) => {
-    const signUpData = get().signUpData;
+    const signData = get().signData;
     set({
-      signUpData: {
-        ...signUpData,
+      signData: {
+        ...signData,
         [targetId]: targetValue,
       },
     });
   },
 
   verifySignData_1: (navigateToUp) => {
-    const signUpData = get().signUpData;
+    const signData = get().signData;
     const nameRegex = /^[가-힣]{2,5}$/;
     const hakbunRegex = /^\d{8}$/;
     const emailRegex = /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/;
 
-    if (!signUpData.name) {
+    if (!signData.name) {
       return alert('이름을 입력해주세요 :D');
     }
-    if (!nameRegex.test(signUpData.name)) {
+    if (!nameRegex.test(signData.name)) {
       return alert('정확한 이름 형식을 사용해주세요 :D');
     }
-    if (!signUpData.hakbun) {
+    if (!signData.hakbun) {
       return alert('학생 번호를 입력해주세요 :D');
     }
-    if (!hakbunRegex.test(signUpData.hakbun)) {
+    if (!hakbunRegex.test(signData.hakbun)) {
       return alert('학생 번호는 8자리 숫자입니다 :D');
     }
-    if (!signUpData.email) {
+    if (!signData.email) {
       return alert('이메일 주소도 입력해주세요 :D');
     }
-    if (!emailRegex.test(signUpData.email)) {
+    if (!emailRegex.test(signData.email)) {
       return alert('이메일 형식을 확인해주세요 :D');
     }
 
     navigateToUp();
   },
 
+  /** sign up */
   verifySignData_2: async () => {
-    const signUpData = get().signUpData;
+    const signData = get().signData;
     const idRegex = /^[A-Za-z0-9]{5,}$/;
     const passwordRegex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[\W_]).{10,}$/;
 
-    if (!signUpData.name || !signUpData.hakbun || !signUpData.email) {
+    if (!signData.name || !signData.hakbun || !signData.email) {
       window.location.replace('/info');
       return alert('가입 오류가 발생하였습니다 :(');
     }
-    if (!signUpData.id) {
+    if (!signData.id) {
       return alert('아이디를 입력해주세요 :D');
     }
-    if (!idRegex.test(signUpData.id)) {
+    if (!idRegex.test(signData.id)) {
       return alert('아이디는 영문 대소문자와 숫자만 사용 가능하게 구성된 5글자 이상이여야 합니다 :D');
     }
-    if (!signUpData.password) {
+    if (!signData.password) {
       return alert('비밀번호를 입력해주세요 :D');
     }
-    if (!passwordRegex.test(signUpData.password)) {
+    if (!passwordRegex.test(signData.password)) {
       return alert('비밀번호는 최소 1개 이상의 영문 대문자와 소문자, 숫자, 특수문자를 포함한 10자리 이상이여야합니다. :D');
     }
-    if (signUpData.password !== signUpData.confirm) {
+    if (signData.password !== signData.confirm) {
       return alert('비밀번호 재확인이 일치하지 않습니다 :(');
     }
-    const newSignUpData = {
-      id: signUpData.id,
-      password: signUpData.password,
-      name: signUpData.name,
-      hakbun: signUpData.hakbun,
-      email: signUpData.email
-    }
+    const newSignData = {
+      id: signData.id,
+      password: signData.password,
+      name: signData.name,
+      hakbun: signData.hakbun,
+      email: signData.email,
+    };
     set({
-      isLoading: true
-    })
+      isLoading: true,
+    });
     try {
-      await auth.signUp(newSignUpData);
+      await auth.signUp(newSignData);
       window.location.replace('/in');
-      return alert('회원가입 성공!')
+      return alert('회원가입 성공!');
     } catch (err) {
       /** Test!!! */ console.log(`verifySignData_2 : ${err}`);
       set({
-        isLoading: false
-      })
-      return alert('axios Error : signup')
+        isLoading: false,
+      });
+      window.location.replace('/info');
+      return alert('axios Error : signup');
+    }
+  },
+  /** sign in */
+  verifySignData_3: async () => {
+    const signData = get().signData;
+
+    if (!signData.id) {
+      return alert('아이디를 입력해주세요 :D');
+    }
+    if (!signData.password) {
+      return alert('비밀번호를 입력해주세요 :D');
+    }
+    const newSignData = {
+      ...signData,
+      id: signData.id,
+      password: signData.password,
+    };
+    set({
+      isLoading: true,
+    });
+    try {
+      // await auth.signUp(newSignData);
+      window.location.replace('/');
+      return alert('로그인 성공!');
+    } catch (err) {
+      /** Test!!! */ console.log(`verifySignData_3 : ${err}`);
+      set({
+        isLoading: false,
+      });
+      window.location.replace('/in');
+      return alert('axios Error : signin');
     }
   },
 }));
